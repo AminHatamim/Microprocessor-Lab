0144227c56655bc5ddaa916f633b0502adb4aabb
aminhatami <minhtmi2102@gmail.com>
2021-10-30 13:28:34 +0330

    commit

--- Ex5/Part1/Src/main.cpp
+++ Ex5/Part1/Src/main.cpp
@@ -2,32 +2,70 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-unsigned char tot_overflow; 
+unsigned int step = 0;
 
-
-interrupt [TIMER0_OVF_vect] void timer0_ovf_isr(void)
+void step_setter()
 {
-    tot_overflow++;
+    switch (step)
+    {
+    case 1:
+        PORTA = 0b0100;
+        break;
+    case 2:
+        PORTA = 0b1101;
+        break;
+    case 3:
+        PORTA = 0b1011;
+        break;
+    case 4:
+        PORTA = 0b0010;
+        step = 0;
+        break;
+    default:
+        PORTA = 0b0000;
+        break;
+    }
 }
 
 
 int main()
 {
 
-    TCCR0 |= (1 << CS00);
+    TCCR0 |= (1 << CS02)|(1 << CS00);
     TCNT0 = 0;
 
-    DDRC = 0;
-    DDRA = 0xFF;
+    DDRC = 0x03;
+    DDRA = 0x0F;
 
     PORTA = 0x00;
 
     TIMSK = 1 << TOIE0; // Enabling Timer0
-    TCCR0 |= 1 << CS01; // Clk/8 Prescaler
+    TCCR0 =0b00000101; // Clk/8 Prescaler
 
     sei();
 
     while (true)
     {
+
+                if(PINC==3){
+            if (TCNT0 >= 5){
+                PORTA =0b0101; 
+                TCNT0 = 0;
+            }
+            if (TCNT0 >= 5){
+                PORTA = 0b0110;
+                TCNT0 = 0; 
+            }
+            if (TCNT0 >= 5){
+                PORTA = 0b1010;
+                TCNT0 = 0;
+            }
+            if (TCNT0 >= 5){
+                PORTA = 0b1001;
+                TCNT0 = 0;
+            }
+        }        
+
     }
+        return 0;
 }
